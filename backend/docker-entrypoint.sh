e2ce9535a8a86da3c7659389298f56c81683d277
Nils Genge <152184930+nilsgenge@users.noreply.github.com>
2026-03-07 11:07:18 +0100

    remove hardcoded app ports

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -5,4 +5,4 @@ echo "Initializing database schema..."
 python init_db.py
 
 echo "Starting SpotiHost..."
-exec uvicorn app.main:app --host 0.0.0.0 --port 8000
+exec uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-8000}
