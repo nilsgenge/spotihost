8e69fd9f6c8a9fef69add32c2b42d8f14f9a9d1f
Nils Genge <152184930+nilsgenge@users.noreply.github.com>
2026-04-30 23:54:43 +0200

    styles: migrate Seperator to seperate scss module

--- frontend/src/components/ui/Separator.tsx
+++ frontend/src/components/ui/Separator.tsx
@@ -1,5 +1,7 @@
+import styles from "./Separator.module.scss";
+
 const Separator = () => {
-  return <hr className="separator" />;
+  return <hr className={styles.separator} />;
 };
 
 export default Separator;
